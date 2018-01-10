87ab583135c78fc50c478f404f49a2317d000314
Matthew Butt <matthew@matthewbutt.com>
2018-01-10 14:39:55 +0000

    Extract database setup to AnalyticsTestJig class

---
+++ WeatherData.III.AcceptanceTests/AnalyticsTestJig.cs
@@ -0,0 +1,59 @@
+﻿using FluentAssertions;
+using Microsoft.Analytics.LocalRun;
+using NUnit.Framework;
+using static System.IO.Directory;
+using static System.IO.File;
+using static System.IO.Path;
+using static NUnit.Framework.TestContext;
+
+namespace WeatherData.III.AcceptanceTests
+{
+    [SetUpFixture]
+    public class AnalyticsTestJig
+    {
+        private static readonly string DataRoot;
+
+        static AnalyticsTestJig()
+        {
+            DataRoot = Combine(CurrentContext.TestDirectory, "UsqlDataRoot");
+        }
+
+        [OneTimeSetUp]
+        public void SetUp()
+        {
+            CreateDirectory(DataRoot);
+            Run(DataDefinitionScript("CreateDatabase.usql"));
+
+            CopyToDataRoot("WeatherData.III.Objects.dll");
+            Run(DataDefinitionScript("RegisterObjectsAssembly.usql"));
+
+            CreateDirectory(Combine(DataRoot, "input", "walkingSkeleton"));
+        }
+
+        public static void Run(string script)
+        {
+            var localRunHelper = new LocalRunHelper
+            {
+                ScriptPath = script,
+                DataRoot = DataRoot
+            };
+
+            localRunHelper.DoRun().Should().BeTrue("script should execute successfully");
+        }
+
+        public static string AnalyticsScript(string scriptName)
+        {
+            return Combine(CurrentContext.TestDirectory, "..", "..", "..", "WeatherData.III.Analytics", scriptName);
+        }
+
+        public static void CopyToDataRoot(string file)
+        {
+            Copy(Combine(CurrentContext.TestDirectory, file), Combine(DataRoot, file), true);
+        }
+
+        private static string DataDefinitionScript(string scriptName)
+        {
+            return Combine(CurrentContext.TestDirectory, "..", "..", "..", "WeatherData.III.DataDefinition", scriptName);
+        }
+    }
+}
--- WeatherData.III.AcceptanceTests/WalkingSkeletonShould.cs
+++ WeatherData.III.AcceptanceTests/WalkingSkeletonShould.cs
@@ -1,61 +1,16 @@
-﻿using FluentAssertions;
-using Microsoft.Analytics.LocalRun;
-using NUnit.Framework;
-using static System.IO.Directory;
-using static System.IO.File;
-using static System.IO.Path;
-using static NUnit.Framework.TestContext;
+﻿using NUnit.Framework;
+using static WeatherData.III.AcceptanceTests.AnalyticsTestJig;
 
 namespace WeatherData.III.AcceptanceTests
 {
     [TestFixture]
     public class WalkingSkeletonShould
     {
-        private string _dataRoot;
-
-        [SetUp]
-        public void SetUp()
-        {
-            _dataRoot = Combine(CurrentContext.TestDirectory, "UsqlDataRoot");
-            CreateDirectory(_dataRoot);
-            CreateDirectory(Combine(_dataRoot, "input", "walkingSkeleton"));
-            CopyToDataRoot("input\\walkingSkeleton\\data.txt");
-            CopyToDataRoot("WeatherData.III.Objects.dll");
-
-            Run(DataDefinitionScript("CreateDatabase.usql"));
-            Run(DataDefinitionScript("RegisterObjectsAssembly.usql"));
-        }
-
         [Test]
         public void Walk()
         {
+            CopyToDataRoot("input\\walkingSkeleton\\data.txt");
             Run(AnalyticsScript("WalkingSkeleton.usql"));
-        }
-
-        private void Run(string script)
-        {
-            var localRunHelper = new LocalRunHelper
-            {
-                ScriptPath = script,
-                DataRoot = _dataRoot
-            };
-
-            localRunHelper.DoRun().Should().BeTrue("script should execute successfully");
-        }
-
-        private static string AnalyticsScript(string scriptName)
-        {
-            return Combine(CurrentContext.TestDirectory, "..", "..", "..", "WeatherData.III.Analytics", scriptName);
-        }
-
-        private void CopyToDataRoot(string file)
-        {
-            Copy(Combine(CurrentContext.TestDirectory, file), Combine(_dataRoot, file), true);
-        }
-
-        private static string DataDefinitionScript(string scriptName)
-        {
-            return Combine(CurrentContext.TestDirectory, "..", "..", "..", "WeatherData.III.DataDefinition", scriptName);
         }
     }
 }
