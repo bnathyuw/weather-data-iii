6a5b66b761bd71eba310af13feea42cabfa216f9
Matthew Butt <matthew@codurance.com>
2018-01-10 12:00:41 +0000

    Create OUTPUT expression
    - Add OUTPUT expression to WalkingSkeleton.usql
    - Create WalkingSkeletonOutputter class.
    - Test fails: An unhandled exception from user code has been reported when invoking the method 'Extract' on the user type 'WeatherData.III.Objects.WalkingSkeletonExtractor'

---
+++ WeatherData.III.Objects/WalkingSkeletonOutputter.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Analytics.Interfaces;
+
+namespace WeatherData.III.Objects
+{
+    public class WalkingSkeletonOutputter : IOutputter
+    {
+        public override void Output(IRow input, IUnstructuredWriter output)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
