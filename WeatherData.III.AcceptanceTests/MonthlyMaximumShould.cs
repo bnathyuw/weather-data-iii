d31a249f7dbea3c474b8d3fab8da9919db4f9363
Matthew Butt <matthew@matthewbutt.com>
2018-01-10 14:31:04 +0000

    Sketch out monthly maximum test

---
+++ WeatherData.III.AcceptanceTests/MonthlyMaximumShould.cs
@@ -0,0 +1,18 @@
+﻿using NUnit.Framework;
+
+namespace WeatherData.III.AcceptanceTests
+{
+    [TestFixture]
+    public class MonthlyMaximumShould
+    {
+        [Test]
+        public void ShowMaximumTemperatureForEachMonthOfTheYear()
+        {
+            // Given some known input data
+            // When the monthly maximum script is run
+            // And the results are read
+            // Then the report has twelve lines
+            // One for each month of the year
+        }
+    }
+}
