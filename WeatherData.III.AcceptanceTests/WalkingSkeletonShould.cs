9a2d7ea27bd298cc327e5b0b95b698a02446f945
Matthew Butt <matthew@codurance.com>
2018-01-10 10:51:21 +0000

    Invoke WalkingSkeleton script
    - Create Analytics U-SQL project
    - Create WalkingSkeleton.usql script
    - Point localRunHelper to this script
    - add '*.user' to .gitignore

--- WeatherData.III.AcceptanceTests/WalkingSkeletonShould.cs
+++ WeatherData.III.AcceptanceTests/WalkingSkeletonShould.cs
@@ -1,6 +1,8 @@
 ﻿using FluentAssertions;
 using Microsoft.Analytics.LocalRun;
 using NUnit.Framework;
+using static System.IO.Path;
+using static NUnit.Framework.TestContext;
 
 namespace WeatherData.III.AcceptanceTests
 {
@@ -10,9 +12,17 @@ namespace WeatherData.III.AcceptanceTests
         [Test]
         public void Walk()
         {
-            var localRunHelper = new LocalRunHelper();
+            var localRunHelper = new LocalRunHelper
+            {
+                ScriptPath = PathToAnalyticsScript("WalkingSkeleton.usql")
+            };
 
             localRunHelper.DoRun().Should().BeTrue("script should execute successfully");
+        }
+
+        private static string PathToAnalyticsScript(string scriptName)
+        {
+            return Combine(CurrentContext.TestDirectory, "..", "..", "..", "WeatherData.III.Analytics", scriptName);
         }
     }
 }
