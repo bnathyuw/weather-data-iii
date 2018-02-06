f95a3c192c64f26576e0ee942ab8cb6c874c2838
Matthew Butt <matthew@matthewbutt.com>
2018-02-06 16:11:21 +0000

    Basic implementation of Outputter

---
+++ WeatherData.III.Objects.Tests/MetOfficeObservationOutputterShould.cs
@@ -0,0 +1,32 @@
+﻿using System.IO;
+using FluentAssertions;
+using Microsoft.Analytics.Interfaces;
+using NSubstitute;
+using NUnit.Framework;
+using static System.Text.Encoding;
+
+namespace WeatherData.III.Objects.Tests
+{
+    [TestFixture]
+    public class MetOfficeObservationOutputterShould
+    {
+        [Test]
+        public void Blah()
+        {
+            var outputter = new MetOfficeObservationOutputter();
+
+            var input = Substitute.For<IRow>();
+            input.Get<string>("location").Returns("Clerkenwell");
+            input.Get<int>("year").Returns(2018);
+            input.Get<int>("month").Returns(2);
+            input.Get<double?>("maximumTemperature").Returns(12.3);
+            var output = Substitute.For<IUnstructuredWriter>();
+            var memoryStream = new MemoryStream();
+            output.BaseStream.Returns(memoryStream);
+            outputter.Output(input, output);
+
+            var bytes = memoryStream.ToArray();
+            UTF8.GetString(bytes, 0, bytes.Length).Should().Be("The maximum temperature in Clerkenwell in 2/2018 was 12.3 degrees Celsius\r\n");
+        }
+    }
+}
--- WeatherData.III.Objects/MetOfficeObservationOutputter.cs
+++ WeatherData.III.Objects/MetOfficeObservationOutputter.cs
@@ -1,4 +1,4 @@
-﻿using System;
+﻿using System.IO;
 using Microsoft.Analytics.Interfaces;
 
 namespace WeatherData.III.Objects
@@ -7,7 +7,14 @@ namespace WeatherData.III.Objects
     {
         public override void Output(IRow input, IUnstructuredWriter output)
         {
-            throw new NotImplementedException();
+            using (var streamWriter = new StreamWriter(output.BaseStream))
+            {
+                var location = input.Get<string>("location");
+                var year = input.Get<int>("year");
+                var month = input.Get<int>("month");
+                var maximumTemperature = input.Get<double?>("maximumTemperature");
+                streamWriter.WriteLine($"The maximum temperature in {location} in {month}/{year} was {maximumTemperature} degrees Celsius");
+            }
         }
     }
 }
