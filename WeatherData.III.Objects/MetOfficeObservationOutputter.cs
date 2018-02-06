e3b7dbf22b5bb4dd039ad1e5edbb161b5096deeb
Matthew Butt <matthew@matthewbutt.com>
2018-02-06 09:08:25 +0000

    Failing for the right reason: real object not implemented

---
+++ WeatherData.III.Objects/MetOfficeObservationOutputter.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using Microsoft.Analytics.Interfaces;
+
+namespace WeatherData.III.Objects
+{
+    public class MetOfficeObservationOutputter : IOutputter
+    {
+        public override void Output(IRow input, IUnstructuredWriter output)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
